935f5d9fe2db3a0bfe04e67f0fc830ede95c18d7
ひよどりBasist <VisualBasist@users.noreply.github.com>
2023-11-04 18:00:27 +0900

    JSONレスポンス

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,22 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Store {
+    id: String,
+    name: String,
+}
+
+async fn get_stores() -> Json<Vec<Store>> {
+    Json(vec![Store {
+        id: "id".to_string(),
+        name: "name".to_string(),
+    }])
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/stores", get(get_stores));
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
