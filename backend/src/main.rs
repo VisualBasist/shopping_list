813cef442ce709db0c14ebd2c30595e03d28fe9c
ひよどりBasist <VisualBasist@users.noreply.github.com>
2023-11-04 19:38:42 +0900

    sqlxで取得

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,34 @@
-use axum::{routing::get, Json, Router};
+use axum::{extract::State, routing::get, Json, Router};
 use serde::Serialize;
+use sqlx::{postgres::PgPoolOptions, FromRow, PgPool};
 
-#[derive(Serialize)]
+#[derive(Serialize, FromRow)]
 struct Store {
     id: String,
     name: String,
 }
 
-async fn get_stores() -> Json<Vec<Store>> {
-    Json(vec![Store {
-        id: "id".to_string(),
-        name: "name".to_string(),
-    }])
+async fn get_stores(State(pool): State<PgPool>) -> Json<Vec<Store>> {
+    Json(
+        sqlx::query_as("SELECT * from stores")
+            .fetch_all(&pool)
+            .await
+            .unwrap(),
+    )
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/stores", get(get_stores));
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres@localhost/shopping_list")
+        .await
+        .expect("can't connect to database");
+
+    let app = Router::new()
+        .route("/stores", get(get_stores))
+        .with_state(pool);
+
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
