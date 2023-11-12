5534a5ba553008bd8b3f2a77a654b1ea586efae4
ひよどりBasist <VisualBasist@users.noreply.github.com>
2023-11-12 16:48:30 +0900

    中央揃え

--- frontend/app/stores/[store_id]/items/page.tsx
+++ frontend/app/stores/[store_id]/items/page.tsx
@@ -1,13 +1,14 @@
 'use client'
-import { CircularProgress } from '@mui/material';
 import useSWR from 'swr';
+import { CircularProgress } from '@mui/material';
+import styles from './page.module.css'
 
 type StoreItem = { id: string, name: string, price?: number };
 
 export default function Page({ params }: { params: { store_id: string } }) {
     const { data: store_items, error, isLoading } = useSWR<StoreItem[], Error>(`http://localhost:8080/stores/${params.store_id}/items`);
     return (
-        <main>
+        <main className={styles.main}>
             {error && <p>{error.message}</p>}
             {isLoading && <CircularProgress />}
             {store_items &&
