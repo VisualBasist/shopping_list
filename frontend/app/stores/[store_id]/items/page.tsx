e3f75cd1da41de6a525dc8a6b3c455722182d1c1
ひよどりBasist <VisualBasist@users.noreply.github.com>
2023-11-13 23:50:28 +0900

    MUIのList

--- frontend/app/stores/[store_id]/items/page.tsx
+++ frontend/app/stores/[store_id]/items/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 import useSWR from 'swr';
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, List, ListItem } from '@mui/material';
 import styles from './page.module.css'
 
 type StoreItem = { id: string, name: string, is_done: boolean, price?: number };
 
-function ListItem(items: StoreItem[]) {
-    return items.map(x => <li key={x.id}>{x.name}{x.price && <span>単価{x.price}</span>}</li>);
+function StoreListItem(items: StoreItem[]) {
+    return items.map(x => <ListItem key={x.id}>{x.name}{x.price && <span>単価{x.price}</span>}</ListItem>);
 }
 
 export default function Page({ params }: { params: { store_id: string } }) {
@@ -18,13 +18,13 @@ export default function Page({ params }: { params: { store_id: string } }) {
             {store_items &&
                 <>
                     <h1>買う</h1>
-                    <ul>
-                        {ListItem(store_items.filter(x => !x.is_done))}
-                    </ul>
+                    <List>
+                        {StoreListItem(store_items.filter(x => !x.is_done))}
+                    </List>
                     <h1>買った</h1>
-                    <ul>
-                        {ListItem(store_items.filter(x => x.is_done))}
-                    </ul>
+                    <List>
+                        {StoreListItem(store_items.filter(x => x.is_done))}
+                    </List>
                 </>
             }
         </main>
