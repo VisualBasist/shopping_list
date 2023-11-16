b9d0ad1a6b0c976d63d95e3a3ca9300872653843
ひよどりBasist <VisualBasist@users.noreply.github.com>
2023-11-17 00:36:07 +0900

    完了かどうか変えれるように

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,10 @@
 use axum::{
     extract::{Path, State},
-    http::HeaderValue,
-    routing::get,
+    http::{HeaderValue, StatusCode},
+    routing::{get, put},
     Json, Router,
 };
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPoolOptions, FromRow, PgPool};
 use tower_http::cors::CorsLayer;
 use uuid::Uuid;
@@ -63,6 +63,26 @@ async fn get_store_items(
     )
 }
 
+#[derive(Deserialize)]
+struct StoreItemStateRequest {
+    is_done: bool,
+}
+
+async fn put_store_item_state(
+    Path((store_id, item_id)): Path<(Uuid, Uuid)>,
+    State(pool): State<PgPool>,
+    Json(StoreItemStateRequest { is_done }): Json<StoreItemStateRequest>,
+) -> StatusCode {
+    sqlx::query("UPDATE store_items SET is_done = $1 WHERE store_id = $2 AND item_id = $3")
+        .bind(is_done)
+        .bind(store_id)
+        .bind(item_id)
+        .execute(&pool)
+        .await
+        .unwrap();
+    StatusCode::NO_CONTENT
+}
+
 #[tokio::main]
 async fn main() {
     let pool = PgPoolOptions::new()
@@ -75,8 +95,16 @@ async fn main() {
         .route("/stores", get(get_stores))
         .route("/items", get(get_items))
         .route("/stores/:store_id/items", get(get_store_items))
+        .route(
+            "/stores/:store_id/items/:item_id/state",
+            put(put_store_item_state),
+        )
         .layer(
-            CorsLayer::new().allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap()),
+            // TODO: 全てのパスで同じにしてしまってるので厳密に
+            CorsLayer::new()
+                .allow_methods([axum::http::Method::GET, axum::http::Method::PUT])
+                .allow_headers([axum::http::header::CONTENT_TYPE])
+                .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap()),
         )
         .with_state(pool);
 
